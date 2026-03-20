138d9f61926f0cba9b715dcdbce916398748e9da
Seobin <ball4777@naver.com>
2026-03-20 22:32:45 +0900

    Add word count functionality in 1152_word_count.c
    Implement word counting from input string.

---
+++ Bronze/1152_word_count.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[1000000];
+
+    fgets(str,sizeof(str),stdin);
+
+    int counts = 0;
+    for (int i=0; str[i]!='\0'; i++)
+    {
+        if (str[i]==' ')
+        {
+            if (i!=0 && str[i+1]!='\n') counts++;
+        }
+    }
+
+    printf("%d",counts+1);
+}
+
+/// 단어 수 세기
